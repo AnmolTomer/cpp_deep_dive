590a3af3bdbfd2192d741212d7f8659811db33a5
Anmol Tomer <anmol3540@gmail.com>
2019-06-23 22:12:57 +0530

    Add getline function example.

--- 02. C++ Basics/01. first.cpp
+++ 02. C++ Basics/01. first.cpp
@@ -1,12 +1,14 @@
 #include <iostream> // Including library in the program
 using namespace std; // Allows programmer to use anything inside std namespace without using std
 // More on namespaces later
+/*
 int main() // Starting point of the program
 {
     cout<<"Hello World!\n"; // Using << insertion operator and enclosing string in double quotes ""
     return 0; // Since written in int therefore returns 0 at the end as it shows successful completion.
     // \n and \t are esscape sequences, specifically these two for new line and tab space respectively.
 }
+ */
 
 // To add two numbers
 /*
@@ -21,3 +23,26 @@ int main()
 
 }
 */
+
+// To take the string input and print it
+/*
+int main()
+{
+    string str;
+    cout<<"Enter your name here : ";
+    cin>>str;
+    cout<<"Hello "<<str<<endl;
+    return 0;
+}
+ */
+
+// To get the entire line input and print it
+
+int main()
+{
+    string str;
+    cout<<"Enter your name here : "; 
+    getline(cin,str); // Function to get the entire line taking input stream and string variable.
+    cout<<"Hello "<<str<<endl;
+    return 0;
+}
