531be60c91690849cafa34e94a562fe853b2cb22
Anmol Tomer <anmol3540@gmail.com>
2019-06-24 00:18:07 +0530

    Add file depicting sizes of file types.

---
+++ 02. C++ Basics/03. size_data_types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char x;
+    int a;
+    float b;
+    long c;
+    double d;
+    long double e;
+    cout<<"Size of char is : "<<sizeof(x)<<" bytes."<<endl;
+    cout<<"Size of int is : "<<sizeof(a)<<" bytes."<<endl;
+    cout<<"Size of float is : "<<sizeof(b)<<" bytes."<<endl;
+    cout<<"Size of long is : "<<sizeof(c)<<" bytes."<<endl;
+    cout<<"Size of double is : "<<sizeof(d)<<" bytes."<<endl;
+    cout<<"Size of long double is : "<<sizeof(e)<<" bytes."<<endl;
+    
+    return 0;
+}
