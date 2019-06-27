33c231275e3cc25587dec5c225d2b5c5a3cfb68b
AnmolTomer <anmol3540@gmail.com>
2019-06-27 22:49:41 +0530

    Add program to find whether a given year is leap year.

---
+++ 03. Conditional Statements/06. leap_year.cpp
@@ -0,0 +1,29 @@
+/*
+author : Anmol Tomer
+email : anmol3540@gmail.com
+
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int year;
+    cout << "Enter the year you want to check for leap year : ";
+    cin >> year;
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+                cout << "Year " << year << " is leap year.";
+            else
+                cout << "Year " << year << " is not a leap year.";
+        }
+        else
+            cout << "Year " << year << " is not a leap year.";
+    }
+    else
+        cout << "Year " << year << " is a leap year.";
+
+    return 0;
+}
