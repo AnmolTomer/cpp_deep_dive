33b1e5b91c9df9aebd53704638fe23516c01c5e5
AnmolTomer <anmol3540@gmail.com>
2019-09-07 23:12:16 +0530

    Add Exception Handling Files.

---
+++ 14. Exception Handling/01. exception_handling_intro.cpp
@@ -0,0 +1,34 @@
+/*
+author : Anmol Tomer
+email : anmol3540@gmail.com
+
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    return 0;
+}
+
+/*
+Exception handling is a feature of OOP also exists in JAVA.
+Exception meaning : 
+There are usually 3 kinds of errors that occur in programming
+1. Syntax Error -> Programmer mistypes or skips writing something or writes it in an improper way then it leads to
+                    syntax error. This is usually caught by compiler on the step where we build the executable file.
+Examples of these types of error are (i) Variable declared but not used, used but not initialized,missing semicolon,
+if-else ladder not made properly , these mostly occurs not due to wrong logic but lack of knowledge of syntax.                    
+2. Logical Error -> Want to do task A but what happens is something unexpected, no errors in syntax but in expected
+    output. These types of errors are difficult to remove as you have to think. Compiler won't help you but Pen and Paper
+    will help for sure along with debugger.
+3. Run-time Error -> Occurs when user is using an application and if user mishandles the app then app may 
+cause error during the runtime of program. There were no errors during development phase but there exists error during
+runtime.
+e.g.(i) Bad Input -> Suppose user needs to give int but is entering a string then it will mess up the program for sure.
+(ii) Internet Connectivity.
+(iii) Deletion of crucial files required for proper functioning of program.
+(iv) Program dependent on a Printer or other hardware resource but that is not connected.
+Thus, lack or inappropriate availability of resources causes Runtime errors which are external factors to the program.
+
+*/
